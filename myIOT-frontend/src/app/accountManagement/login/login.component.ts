234fcf98441d72c262649069cdea3814335e68cc
Carvalho <megalitikgamer@gmail.com>
2022-01-31 01:40:10 +0000

    Created Login service

--- myIOT-frontend/src/app/accountManagement/login/login.component.ts
+++ myIOT-frontend/src/app/accountManagement/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-login',
@@ -9,15 +10,21 @@ import { FormBuilder, Validators } from '@angular/forms';
 export class LoginComponent implements OnInit {
 
   public loginForm = this.formBuilder.group({
-    email:['', [Validators.email, Validators.required]],
-    password:['', Validators.required]
+    email: ['', [Validators.email, Validators.required]],
+    password: ['', Validators.required]
   });
-  constructor(private formBuilder:FormBuilder) { }
+  constructor(private formBuilder: FormBuilder, private userService: UserService) { }
 
   ngOnInit(): void {
   }
   onSubmit() {
     console.log("On submit...")
-  }
+    let email = this.loginForm.controls["email"].value;
+    let password = this.loginForm.controls["password"].value;
 
+
+    this.userService.Login(email, password).subscribe((data) => {
+      console.log("Response: ", data);
+    })
+  }
 }

aa343d212a08846f998f248a71c6386df658c933
João Carvalho <colaboradorioss3@impresa.pt>
2023-01-12 16:19:56 +0000

    Built structure of Sensors

--- myIOT-frontend/src/app/userPanel/sensors/sensors.component.ts
+++ myIOT-frontend/src/app/userPanel/sensors/sensors.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { ChartDataSets, ChartOptions, ChartType } from 'chart.js';
 
 @Component({
   selector: 'app-sensors',
@@ -7,8 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SensorsComponent implements OnInit {
 
-  constructor() { }
+  deviceForm!: FormGroup;
+  temperatura = '23'
+  public lineChartColors: any[] = [
+    {
+      borderColor: 'black',
+      backgroundColor: 'rgba(255,255,0,0.28)',
+    },
+  ];
+  public lineChartData: any[] = [
+    { data: [85, 72, 78, 75, 77, 75], label: 'Crude oil prices' },
+  ];
+  public lineChartOptions = {
+    responsive: true,
+  };
+  public lineChartLabels: any[] = ['January', 'February', 'March', 'April', 'May', 'June'];
+  public lineChartLegend = false;
+  lineChartType: ChartType = 'line';
+
+  constructor(private http: HttpClient, private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.deviceForm = this.fb.group({
+      deviceId: '',
+      chartType: 'line',
+      showLegend: true
+    });
   }
 }

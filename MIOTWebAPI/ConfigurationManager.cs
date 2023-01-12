462739442aa0de8369bd27a035b538f35ee21b43
João Carvalho <colaboradorioss3@impresa.pt>
2023-01-12 17:40:33 +0000

    Created ConfigurationManager to access appsettings

---
+++ MIOTWebAPI/ConfigurationManager.cs
@@ -0,0 +1,15 @@
+
+
+
+namespace MIOTWebAPI {
+    static class ConfigurationManager {
+        public static IConfiguration AppSetting {
+
+            get;
+        }
+        static ConfigurationManager() {
+
+            AppSetting = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json").Build();
+        }
+    }
+}
